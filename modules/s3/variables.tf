4d35dbf99ab8610ae37017466f3d0aa7edc1abd7
rodrigo <rodrigo.ribeiro03@hotmail.com>
2026-03-07 16:52:07 -0300

    ajustandop projeto

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -1,5 +1,8 @@
 variable "name_prefix"          { type = string }
 variable "environment"           { type = string }
 variable "account_id"            { type = string }
-variable "lifecycle_bronze_days" { type = number; default = 90 }
+variable "lifecycle_bronze_days" {
+  type    = number
+  default = 90
+}
 variable "lifecycle_silver_days" { type = number; default = 180 }
