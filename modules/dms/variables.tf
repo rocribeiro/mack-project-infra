77efa651a753f52f5ae9c50ed8996f2286a5509f
rodrigo <rodrigo.ribeiro03@hotmail.com>
2026-03-07 16:56:30 -0300

    refactor: format variable declarations for consistency across modules

--- modules/dms/variables.tf
+++ modules/dms/variables.tf
@@ -1,8 +1,18 @@
-variable "name_prefix"               { type = string }
-variable "environment"                { type = string }
-variable "dms_role_arn"               { type = string }
-variable "s3_bucket_sor_arn"          { type = string }
-variable "s3_bucket_sor_name"         { type = string }
+variable "name_prefix" {
+  type = string
+}
+variable "environment" {
+  type = string
+}
+variable "dms_role_arn" {
+  type = string
+}
+variable "s3_bucket_sor_arn" {
+  type = string
+}
+variable "s3_bucket_sor_name" {
+  type = string
+}
 variable "replication_instance_class" {
   type    = string
   default = "dms.t3.micro"
