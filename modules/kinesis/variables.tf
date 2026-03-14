d02be9dbaf0afecabe841c49fdd0de7a80531e1e
rodrigo <rodrigo.ribeiro03@hotmail.com>
2026-03-14 10:17:13 -0300

    style: reformat variable declarations for consistency across modules

--- modules/kinesis/variables.tf
+++ modules/kinesis/variables.tf
@@ -1,9 +1,39 @@
-variable "name_prefix"              { type = string }
-variable "environment"               { type = string }
-variable "kinesis_role_arn"          { type = string }
-variable "s3_bucket_sor_arn"         { type = string }
-variable "s3_bucket_sor_name"        { type = string }
-variable "shard_count"               { type = number; default = 1 }
-variable "retention_hours"           { type = number; default = 24 }
-variable "firehose_buffer_size_mb"   { type = number; default = 64 }
-variable "firehose_buffer_interval_sec" { type = number; default = 300 }
+variable "name_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "kinesis_role_arn" {
+  type = string
+}
+
+variable "s3_bucket_sor_arn" {
+  type = string
+}
+
+variable "s3_bucket_sor_name" {
+  type = string
+}
+
+variable "shard_count" {
+  type    = number
+  default = 1
+}
+
+variable "retention_hours" {
+  type    = number
+  default = 24
+}
+
+variable "firehose_buffer_size_mb" {
+  type    = number
+  default = 64
+}
+
+variable "firehose_buffer_interval_sec" {
+  type    = number
+  default = 300
+}
