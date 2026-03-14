d6f7845198cd28f2541d43d02b22cec96ddf4408
rodrigo <rodrigo.ribeiro03@hotmail.com>
2026-03-14 09:40:59 -0300

    ajustando estrturura variables

--- modules/glue/variables.tf
+++ modules/glue/variables.tf
@@ -1,10 +1,42 @@
-variable "name_prefix"       { type = string }
-variable "environment"        { type = string }
-variable "glue_role_arn"      { type = string }
-variable "s3_bucket_sor"      { type = string }
-variable "s3_bucket_sot"      { type = string }
-variable "s3_bucket_spec"     { type = string }
-variable "s3_scripts_bucket"  { type = string }
-variable "glue_worker_type"   { type = string; default = "G.1X" }
-variable "glue_number_of_workers" { type = number; default = 2 }
-variable "glue_max_retries"   { type = number; default = 1 }
+variable "name_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "glue_role_arn" {
+  type = string
+}
+
+variable "s3_bucket_sor" {
+  type = string
+}
+
+variable "s3_bucket_sot" {
+  type = string
+}
+
+variable "s3_bucket_spec" {
+  type = string
+}
+
+variable "s3_scripts_bucket" {
+  type = string
+}
+
+variable "glue_worker_type" {
+  type    = string
+  default = "G.1X"
+}
+
+variable "glue_number_of_workers" {
+  type    = number
+  default = 2
+}
+
+variable "glue_max_retries" {
+  type    = number
+  default = 1
+}
