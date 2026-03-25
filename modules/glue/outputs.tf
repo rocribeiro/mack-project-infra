cc18d8a448a479d0d6822be9047e94cfe3d1df31
Julio Stein <juliostein90@gmail.com>
2026-03-25 20:27:32 -0300

    ajuste output database

--- modules/glue/outputs.tf
+++ modules/glue/outputs.tf
@@ -1,5 +1,5 @@
-output "database_name" { value = aws_glue_catalog_database.sot.name }
-output "database_name" { value = aws_glue_catalog_database.spec.name }
+output "database_name_sot" { value = aws_glue_catalog_database.sot.name }
+output "database_name_spec" { value = aws_glue_catalog_database.spec.name }
 output "job_bronze_to_silver_name" { value = aws_glue_job.bronze_to_silver.name }
 output "job_silver_to_gold_name" { value = aws_glue_job.silver_to_gold.name }
 output "workflow_name" { value = aws_glue_workflow.pipeline.name }
